1fab6c3440e28547edd8846a4b341f5ffd4759c9
swapnens <swapnens@andrew.cmu.edu>
2019-03-15 08:57:44 +0400

    Testing

--- Day 6 - Number Theory/library.cpp
+++ Day 6 - Number Theory/library.cpp
@@ -1,14 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define dMult(n,c) (n/gcd(c,n)) // number of distinct multiples of c modulo n
+//only iff gcd(c,n) = 1, there are n different numbers modulo n
 
 long long GCD (long long x, long long y)
 {
-    //gcd(x,y);
-    if(x<y) swap(x,y);
-    if(y<1)
-        return x;
-    return GCD(y,x%y);
+    return __gcd(x,y);
+    // if(x<y) swap(x,y);
+    // if(y<1)
+    //     return x;
+    // return GCD(y,x%y);
 }
 
 int main()
@@ -17,5 +19,6 @@ int main()
     x = 123;
     y = 27;
     cout << GCD(x,y) << endl;
+    while(1);
     return 0;
 }
